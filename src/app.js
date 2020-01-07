72c106421c5f3550e1e8b8e00e47d31b6b2287d4
Patrick Gustafson <patrickgus@charter.net>
2020-01-06 18:02:06 -0800

    create error-handling middleware function

--- src/app.js
+++ src/app.js
@@ -16,4 +16,15 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
 module.exports = app;
