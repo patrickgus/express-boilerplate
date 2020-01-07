93c8a23047f79f4232b349d2ed2da314ff061a0c
Patrick Gustafson <patrickgus@charter.net>
2020-01-06 18:08:05 -0800

    import NODE_ENV

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = process.env.NODE_ENV === "production" ? "tiny" : "common";
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
@@ -18,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.use((error, req, res, next) => {
   let response;
-  if (process.env.NODE_ENV === "production") {
+  if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
     console.error(error);
